e13ea120df22f2a439ea4ac4c6e206521cf8eca9
Vaishnavi Saravanan <75262426+VaishnaviSaravanan2801@users.noreply.github.com>
2020-12-03 11:44:07 -0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>Personal Information Chart</title>	
+</head>
+<body>
+<center>
+<h1>Personal Information Chart</h1>
+<h2>User Login</h2>
+<style>
+body {
+  background-image: url('https://img.freepik.com/free-photo/blurred-bokeh-background_36923-877.jpg?size=626&ext=jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+<img src="https://cdn2.vectorstock.com/i/thumb-large/25/26/user-login-or-access-authentication-icon-vector-6712526.jpg" width="150" height="150" align="center">
+<table>
+<tr>
+<td><h3>Email Id:</h3></td>
+<td><input type="email" name="email"></td>
+</tr>
+<tr>
+<td><h3>Username:</h3></td>
+<td><input type="text" name="username"></td><br>
+</tr>
+<tr>
+<td><h3>Password:</h3></td>
+<td><input type="password" name="pwd"></td><br>
+<tr>
+<td><br><input type="checkbox" name="remember">Remember Me</td><br>
+<td><br><center><a href="Pci.html"><input type="button" name="login" value="Login" width="200" height="200"/></a></td>
+</tr>
+</center>
+</center>
+</table>
+</body>
+</html>

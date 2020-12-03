d2c2e269b60dd06a77670aebae96d4116825d302
Vaishnavi Saravanan <75262426+VaishnaviSaravanan2801@users.noreply.github.com>
2020-12-03 11:42:50 -0800

    Create accomplish.js

---
+++ accomplish.js
@@ -0,0 +1,51 @@
+<html>
+<head>
+<title>Personal Information Chart</title>	
+</head>
+<body>
+<style>
+body {
+  background-image: url('https://cdn2.slidemodel.com/wp-content/uploads/7243-01-low-poly-background-16x9-1.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+<center>
+<h1><center>&ensp; Personal Information Chart</h1>
+<h2><center>Accomplishment List</center></h2>
+&ensp;<img src="https://cdn4.vectorstock.com/i/thumb-large/37/13/knowledge-and-learning-concept-vector-2843713.jpg" width="250" height="250" align="left" />
+<img src="https://cdn3.vectorstock.com/i/thumb-large/73/42/icons-for-arts-vector-997342.jpg" width="250" height="250" align="right" />
+<table>
+<tr><td><h2>Courses:</h2>
+<h3>Chatbot Development</h3>
+<h3>Programming in Java</h3>
+<h3>Programming in Python</h3>
+<h3>Digital Marketing</h3>
+<h3>FrontEnd Web Development</h3></td>
+<td><h2>Area of Interest:</h2>
+<h3>Debate & Oration</h3>
+<h3>Fine Arts</h3>
+<h3>Designing & Animation</h3></td><br></tr>
+<tr>
+	<td>
+		<h2><br>Certificate Links:</h2>
+		<h3>Build Your Own Chatbot:</h3><td><br><br><br><br>&ensp; <a href="https://www.youracclaim.com/badges/5996d9d2-1be7-4ab5-be1c-11936eed3284/linked_in_profile">https://www.udemy.com/certificate</a></td></tr>	
+<tr>
+	<td>
+		<h3><br>Data Science for Business :</h3></td><td><br>&ensp; <a href="https://www.youracclaim.com/badges/7a641c9c-b940-4efd-a13e-1277ac9bf38c/linked_in_profile">https://www.ibm.in/certificate</a></td></tr>
+<tr>
+	<td><h3><br>Data Visualization Using Python:</h3><td><br>&ensp; <a href="https://www.youracclaim.com/badges/8f23fd4b-ac75-4098-b69f-f0d0b8a47a55/linked_in_profile">https://www.udemy.com/certificate</a></td></tr>
+<tr>
+	<td><h3><br>Fundamentals of Digital Marketing:</h3><td><br>&ensp; <a href="https://learndigital.withgoogle.com/digitalunlocked/course/digital-marketing/certificate.pdf/">https://www.google-digital-lock/certificate</a></td></tr>
+<tr>
+	<td><h3><br>Complete Front-End Web Development <br> from Udemy:</h3><td><br>&ensp; <a href="https://www.udemy.com/certificate/UC-582612d8-586b-4423-ba2e-49345fb44f86/">https://www.udemy.com/certificate</a></td></tr>
+<tr>
+	<td>
+		<br>&ensp;<center><div class="tab">
+		 <center><button class="tablinks" onclick="document.location='Pci.html'" align="right">Back</button></center>
+  </div></center>
+</td></tr>
+</center>
+</body>
+</html>

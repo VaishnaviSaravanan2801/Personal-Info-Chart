8ac3c0a7102a4474cd8b3f54099ad70fb3cbaf0c
Vaishnavi Saravanan <75262426+VaishnaviSaravanan2801@users.noreply.github.com>
2020-12-03 11:47:35 -0800

    Create skill.js

---
+++ skill.js
@@ -0,0 +1,56 @@
+<html>
+<head>
+<title>Personal Information Chart</title>	
+</head>
+<body>
+<style>
+body {
+  background-image: url('https://cdn2.slidemodel.com/wp-content/uploads/7243-01-low-poly-background-16x9-1.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+<h1><center>Personal Information Chart</h1>
+<h2><b><center>Skill Set</b></h2></center>
+	<div class="tab">
+		 <button class="tablinks" onclick="document.location='Pci.html'">Back</button>
+  </div>
+  <table>
+  <tr><td>
+<h2><b> &ensp; Programming Language</b></h2>
+<center>
+<h3>Java</h3>
+<h3>C & C++</h3>
+<h3>PHP</h3>
+<h3>Python</h3>
+<h3>JavaScript</h3></td>
+</center>
+<td><h2><b><br> &ensp; Industry Knowledge</h2></b>
+<center>
+<h3>Machine Learning</h3>
+<h3>Data Analytics</h3>
+<h3>Data Visualization</h3>
+<h3>Project Managemnent</h3>
+<h3>Data Structures</h3>
+<h3>Artificial Intelligence</h3></td></center>
+<td><h2><b>&ensp; Interpersonal Skills</h2></b>
+<center>
+<h3>Presentation Skills</h3>
+<h3>Team Leadership</h3>
+<h3>Problem Solving</h3>
+<h3>Public Speaking</h3></td></center>
+<td><h2><b>&ensp; Tools & Technologies</h2></b>
+<center>
+<h3>Microsoft Powerpoint</h3>
+<h3>Microsoft Word</h3>
+<h3>Adobe Photoshop</h3></td></center>
+<td><h2><b>&ensp; Development Skill</h2></b>
+<center>
+<h3>Chatbot development</h3>
+<h3>Adobe Flash</h3></td></center>
+</tr>
+</table>
+</center>
+</body>
+</html>
